305eafd7355f6a85abe4f462ef1bfe470eecedd8
Simbarashe Mupa <simba.mupa@centricdata.net>
2019-03-18 14:44:27 +0200

    1st

---
+++ AdminLTE1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+
+namespace AdminLTE1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewDiscount()
+        {
+            return View();
+        }
+        public ActionResult NewLoading()
+        {
+            return View();
+        }
+        public ActionResult NewRating()
+        {
+            return View();
+        }
+        public ActionResult ReInsuranceCompany()
+        {
+            return View();
+        }
+        public ActionResult ReInsuranceModel()
+        {
+            return View();
+        }
+        public ActionResult Risk()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+    }
+}
